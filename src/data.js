a21caa30b83dc54b6714600d80d8209fe479eebd
Jeu <jeunettemanzo@gmail.com>
2019-03-15 09:37:34 -0600

    fix: errors href

--- src/data.js
+++ src/data.js
@@ -1,25 +1,25 @@
-// var listMovies = [];
-let element = document.getElementById('element');
-let list = '';
-let i = [];
-
-
-
-const listMovies = fromSpace.forEach(function(name){
-  api(name).then((response) => console.log(response));
-  // API(name).then(element.innerHTML = `<span>${}</span>`);
-})
-
-
-// function mostrar(a) {
-//   element.innerHTML = `<span>${a}</span>`
-// }
+// // var listMovies = [];
+// let element = document.getElementById('element');
+// let list = '';
+// let i = [];
 //
-// mostrar("pruebas")
-
-  async function api(urlforanea){
-  const url = 'http://www.omdbapi.com/?apikey=7250cff9&t=$';
-  const response = await fetch(url + urlforanea);
-  const data = await response.json()
-  return data.Poster
-}
+//
+//
+// const listMovies = fromSpace.forEach(function(name){
+//   api(name).then((response) => console.log(response));
+//   // API(name).then(element.innerHTML = `<span>${}</span>`);
+// })
+//
+//
+// // function mostrar(a) {
+// //   element.innerHTML = `<span>${a}</span>`
+// // }
+// //
+// // mostrar("pruebas")
+//
+//   async function api(urlforanea){
+//   const url = 'http://www.omdbapi.com/?apikey=7250cff9&t=$';
+//   const response = await fetch(url + urlforanea);
+//   const data = await response.json()
+//   return data.Poster
+// }
